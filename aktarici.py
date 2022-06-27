d41b01f630600c9996bba8172a58f3fd7999636c
muhammedemiraysan <89260017+muhammedemiraysan@users.noreply.github.com>
2022-06-27 14:38:13 +0300

    Update aktarici.py

--- aktarici.py
+++ aktarici.py
@@ -1,10 +1,9 @@
 import serial
 import time
-arac_arduino = serial.Serial(port="COM3",baudrate = 115200)
-kumanda_arduino = serial.Serial(port="COM9",baudrate = 115200)
+arac_arduino = serial.Serial(port="COM20",baudrate = 9600)
+kumanda_arduino = serial.Serial(port="COM9",baudrate = 9600)
 while True:
     x = kumanda_arduino.readline()
     if x is not None:
         arac_arduino.write(bytes(str(x), 'utf-8'))
-        print(x)
-            
+        print(x)    
